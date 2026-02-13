c4994adc4dc09fb7dbe25ca3877594c93faee3f4
Bayojid AI <bbostami385@gmail.com>
2026-02-14 01:33:57 +0600

    Remove unnecessary whitespace in index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.post("/chat", async (req, res) => {
   try {
     const { message } = req.body;
